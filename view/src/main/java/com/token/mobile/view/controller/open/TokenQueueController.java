aa82526b0b17e80537d11e097fe55d8327d76698
hth <hth@users.noreply.github.com>
2017-02-28 20:00:07 +0530

    Added json response

--- view/src/main/java/com/token/mobile/view/controller/open/TokenQueueController.java
+++ view/src/main/java/com/token/mobile/view/controller/open/TokenQueueController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
+import com.token.domain.json.JsonBooleanResponse;
 import com.token.domain.json.JsonQueue;
 import com.token.domain.json.JsonToken;
 import com.token.domain.json.JsonTokenAndQueue;
@@ -170,7 +171,7 @@ public class TokenQueueController {
             value = "/abort/{codeQR}",
             produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
     )
-    public Boolean abortQueue(
+    public JsonBooleanResponse abortQueue(
             @RequestHeader ("X-R-DID")
             ScrubbedInput did,
 
