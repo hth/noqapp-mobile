1ed4aa5fe61c91e5a95aa86ea5d9d08c0c6bf5e1
hth <hth@users.noreply.github.com>
2017-03-12 16:08:19 +0530

    added create date

--- service/src/main/java/com/token/mobile/service/TokenQueueMobileService.java
+++ service/src/main/java/com/token/mobile/service/TokenQueueMobileService.java
@@ -52,7 +52,8 @@ public class TokenQueueMobileService {
                 .setTopic(bizStore.getTopic())
                 .setServingNumber(tokenQueue.getCurrentlyServing())
                 .setLastNumber(tokenQueue.getLastNumber())
-                .setQueueStatus(tokenQueue.getQueueStatus());
+                .setQueueStatus(tokenQueue.getQueueStatus())
+                .setCreateDate(tokenQueue.getCreated());
     }
 
     public JsonToken joinQueue(String codeQR, String did, String rid) {
