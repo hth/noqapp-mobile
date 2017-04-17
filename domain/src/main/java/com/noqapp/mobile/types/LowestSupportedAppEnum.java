d51abd97dc19e3b5f33eb6c40e89587ef1319ed6
hth <hth@users.noreply.github.com>
2017-04-17 17:01:48 +0530

    added code to to check for app version

---
+++ domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
@@ -0,0 +1,46 @@
+package com.noqapp.mobile.types;
+
+/**
+ * User: hitender
+ * Date: 4/17/17 3:24 PM
+ */
+
+import com.noqapp.domain.types.DeviceTypeEnum;
+
+/**
+ * API's are never old. App installed on device is old.
+ *
+ * User: hitender
+ * Date: 1/19/17 12:00 PM
+ */
+public enum LowestSupportedAppEnum {
+
+    /** List lowest supported version of iPhone and Android app. */
+    VI("1.0.0", 100, DeviceTypeEnum.I),
+    VA("1.0.0", 100, DeviceTypeEnum.A);
+
+    private String appVersion;
+    private int appVersionNumber;
+    private DeviceTypeEnum deviceType;
+
+    LowestSupportedAppEnum(String appVersion, int appVersionNumber, DeviceTypeEnum deviceType) {
+        this.appVersion = appVersion;
+        this.appVersionNumber = appVersionNumber;
+        this.deviceType = deviceType;
+    }
+
+    public int getAppVersionNumber() {
+        return appVersionNumber;
+    }
+
+    public static boolean isLessThanLowestSupportedVersion(DeviceTypeEnum deviceType, int appVersionNumber) {
+        boolean notSupported = false;
+        for (LowestSupportedAppEnum lowestSupportedAPI : LowestSupportedAppEnum.values()) {
+            if (lowestSupportedAPI.deviceType == deviceType && lowestSupportedAPI.appVersionNumber > appVersionNumber) {
+                notSupported = true;
+            }
+        }
+
+        return notSupported;
+    }
+}
