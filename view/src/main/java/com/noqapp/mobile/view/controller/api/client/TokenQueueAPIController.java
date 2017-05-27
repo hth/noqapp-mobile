b90bd5a8ec00955570d28bb597a36a0f0722b7a3
hth <hth@users.noreply.github.com>
2017-05-27 14:27:03 +0530

    added remote join api

--- view/src/main/java/com/noqapp/mobile/view/controller/api/client/TokenQueueAPIController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/client/TokenQueueAPIController.java
@@ -339,7 +339,7 @@ public class TokenQueueAPIController {
             value = "/remote/{codeQR}",
             produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
     )
-    public JsonQueue remoteScanQueue(
+    public String remoteScanQueueState(
             @RequestHeader ("X-R-DID")
             ScrubbedInput did,
 
@@ -358,7 +358,6 @@ public class TokenQueueAPIController {
             HttpServletResponse response
     ) throws IOException {
         LOG.info("On remote scan get state did={} dt={} codeQR={}", did, deviceType, codeQR);
-
         String rid = authenticateMobileService.getReceiptUserId(mail.getText(), auth.getText());
         if (authorizeRequest(response, rid)) return null;
 
@@ -367,20 +366,69 @@ public class TokenQueueAPIController {
             return null;
         }
 
-        int remoteScanCount = inviteService.getRemoteScanCount(rid);
-        if (remoteScanCount > 0) {
+        try {
             JsonQueue jsonQueue = tokenQueueMobileService.findTokenState(codeQR.getText());
-            if (jsonQueue != null) {
-                inviteService.deductRemoteScanCount(rid);
-                return jsonQueue;
-            }
-        } else {
-            LOG.info("Cannot do remote scan as none available remoteScanCount={}", remoteScanCount);
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ManageQueueController.UNAUTHORIZED);
+            int remoteScanCount = inviteService.getRemoteScanCount(rid);
+            jsonQueue.setRemoteJoin(remoteScanCount);
+            return jsonQueue.asJson();
+        } catch (Exception e) {
+            LOG.error("Failed getting queue state rid={}, reason={}", rid, e.getLocalizedMessage(), e);
+            return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+        }
+    }
+
+    /**
+     * Join the queue remotely. Only for registered user and who has number of remote join available.
+     *
+     * @param did
+     * @param deviceType
+     * @param codeQR
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    @Timed
+    @ExceptionMetered
+    @RequestMapping (
+            method = RequestMethod.POST,
+            value = "/remote/queue/{codeQR}",
+            produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
+    )
+    public String remoteJoinQueue(
+            @RequestHeader ("X-R-DID")
+            ScrubbedInput did,
+
+            @RequestHeader ("X-R-DT")
+            ScrubbedInput deviceType,
+
+            @RequestHeader ("X-R-MAIL")
+            ScrubbedInput mail,
+
+            @RequestHeader ("X-R-AUTH")
+            ScrubbedInput auth,
+
+            @PathVariable ("codeQR")
+            ScrubbedInput codeQR,
+
+            HttpServletResponse response
+    ) throws IOException {
+        LOG.info("Join queue did={} dt={} codeQR={}", did, deviceType, codeQR);
+        String rid = authenticateMobileService.getReceiptUserId(mail.getText(), auth.getText());
+        if (authorizeRequest(response, rid)) return null;
+
+        if (!tokenQueueMobileService.getBizService().isValidCodeQR(codeQR.getText())) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Invalid token");
             return null;
         }
 
-        return null;
+        try {
+            String jsonQueue = tokenQueueMobileService.joinQueue(codeQR.getText(), did.getText(), rid).asJson();
+            inviteService.deductRemoteScanCount(rid);
+            return jsonQueue;
+        } catch (Exception e) {
+            LOG.error("Failed joining queue rid={}, reason={}", rid, e.getLocalizedMessage(), e);
+            return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+        }
     }
 
     static boolean authorizeRequest(HttpServletResponse response, String rid) throws IOException {
