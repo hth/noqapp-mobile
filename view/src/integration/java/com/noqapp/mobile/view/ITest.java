9f7cf7c88c981faee9658b2441d3b4fe1c81c27d
hth <hth@users.noreply.github.com>
2017-12-10 18:12:20 +0530

    added fcm token

--- view/src/integration/java/com/noqapp/mobile/view/ITest.java
+++ view/src/integration/java/com/noqapp/mobile/view/ITest.java
@@ -77,6 +77,7 @@ import java.util.UUID;
 public class ITest extends RealMongoForITest {
 
     protected String did;
+    protected String fcmToken;
     protected String deviceType;
 
     protected AccountService accountService;
@@ -125,6 +126,7 @@ public class ITest extends RealMongoForITest {
         MockitoAnnotations.initMocks(this);
 
         did = UUID.randomUUID().toString();
+        fcmToken = UUID.randomUUID().toString();
         deviceType = DeviceTypeEnum.A.getName();
 
         userAccountManager = new UserAccountManagerImpl(getMongoTemplate());
