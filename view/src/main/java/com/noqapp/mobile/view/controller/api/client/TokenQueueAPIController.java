4d11e8192a35c34a5f55199e3aeb87d0b10c18e6
hth <hth@users.noreply.github.com>
2018-04-16 17:05:33 +0530

    added set for unique business

--- view/src/main/java/com/noqapp/mobile/view/controller/api/client/TokenQueueAPIController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/client/TokenQueueAPIController.java
@@ -179,7 +179,7 @@ public class TokenQueueAPIController {
         }
 
         try {
-            return tokenQueueMobileService.findAllBizStoreByBizNameCodeQR(codeQR.getText()).asJson();
+            return tokenQueueMobileService.findAllBizStoreByBizNameCodeQRSet(codeQR.getText()).asJson();
         } catch (Exception e) {
             LOG.error("Failed getting all queue state qid={}, reason={}", qid, e.getLocalizedMessage(), e);
             apiHealthService.insert(
