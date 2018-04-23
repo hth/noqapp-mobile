897e35eecc479643e7b2394abf7bce6ef04b3d4a
hth <hth@users.noreply.github.com>
2018-04-23 23:40:10 +0530

    fixed method for updating profile

--- view/src/main/java/com/noqapp/mobile/view/controller/api/client/ClientProfileAPIController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/client/ClientProfileAPIController.java
@@ -4,11 +4,15 @@ import com.noqapp.common.utils.Formatter;
 import com.noqapp.common.utils.ParseJsonStringToMap;
 import com.noqapp.domain.UserAccountEntity;
 import com.noqapp.domain.UserProfileEntity;
+import com.noqapp.domain.flow.RegisterUser;
 import com.noqapp.domain.json.JsonResponse;
+import com.noqapp.domain.types.AddressOriginEnum;
 import com.noqapp.domain.types.DeviceTypeEnum;
+import com.noqapp.domain.types.GenderEnum;
 import com.noqapp.health.domain.types.HealthStatusEnum;
 import com.noqapp.health.service.ApiHealthService;
 import com.noqapp.mobile.common.util.ErrorEncounteredJson;
+import com.noqapp.mobile.common.util.ExtractFirstLastName;
 import com.noqapp.mobile.domain.JsonProfile;
 import com.noqapp.mobile.service.AccountMobileService;
 import com.noqapp.mobile.service.AuthenticateMobileService;
@@ -19,6 +23,7 @@ import com.noqapp.service.InviteService;
 import com.noqapp.service.UserProfilePreferenceService;
 import com.noqapp.common.utils.ScrubbedInput;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +49,7 @@ import java.util.Set;
 
 import static com.noqapp.common.utils.CommonUtil.AUTH_KEY_HIDDEN;
 import static com.noqapp.common.utils.CommonUtil.UNAUTHORIZED;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MOBILE;
 import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MOBILE_JSON;
 import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.SEVERE;
 import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.USER_EXISTING;
@@ -158,12 +164,100 @@ public class ClientProfileAPIController {
             return null;
         }
 
-        UserAccountEntity userAccount = accountService.findByQueueUserId(qid);
-        UserProfileEntity userProfile = userProfilePreferenceService.findByQueueUserId(userAccount.getQueueUserId());
-        int remoteJoin = inviteService.getRemoteJoinCount(userAccount.getQueueUserId());
-        LOG.info("Remote join available={}", remoteJoin);
+        Map<String, ScrubbedInput> map;
+        try {
+            map = ParseJsonStringToMap.jsonStringToMap(registrationJson);
+        } catch (IOException e) {
+            LOG.error("Could not parse json={} reason={}", registrationJson, e.getLocalizedMessage(), e);
+            return ErrorEncounteredJson.toJson("Could not parse JSON", MOBILE_JSON);
+        }
 
-        return JsonProfile.newInstance(userProfile, remoteJoin).asJson();
+        Map<String, String> errors;
+        try {
+            if (map.isEmpty()) {
+                /* Validation failure as there is no data in the map. */
+                return ErrorEncounteredJson.toJson(accountClientValidator.validate(
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null));
+            } else {
+                Set<String> unknownKeys = invalidElementsInMapDuringUpdate(map);
+                if (!unknownKeys.isEmpty()) {
+                    /* Validation failure as there are unknown keys. */
+                    return ErrorEncounteredJson.toJson("Could not parse " + unknownKeys, MOBILE_JSON);
+                }
+
+                /* Required. But not changing the phone number. For that we have migrate API. */
+                UserProfileEntity userProfile = accountService.findProfileByQueueUserId(qid);
+                String phone = userProfile.getPhone();
+                /* Required. */
+                String firstName = WordUtils.capitalize(map.get(AccountMobileService.ACCOUNT_UPDATE.FN.name()).getText());
+                String lastName = null;
+                if (StringUtils.isNotBlank(firstName)) {
+                    ExtractFirstLastName extractFirstLastName = new ExtractFirstLastName(firstName);
+                    firstName = extractFirstLastName.getFirstName();
+                    lastName = extractFirstLastName.getLastName();
+                }
+
+                ScrubbedInput address = map.get(AccountMobileService.ACCOUNT_UPDATE.AD.name());
+                ScrubbedInput birthday = map.get(AccountMobileService.ACCOUNT_UPDATE.BD.name());
+                /* Required. */
+                String gender = map.get(AccountMobileService.ACCOUNT_UPDATE.GE.name()).getText();
+                /* Required. */
+                String countryShortName = userProfile.getCountryShortName();
+                /* Required. */
+                ScrubbedInput timeZone = map.get(AccountMobileService.ACCOUNT_UPDATE.TZ.name());
+
+                errors = accountClientValidator.validate(
+                        phone,
+                        map.get(AccountMobileService.ACCOUNT_UPDATE.FN.name()).getText(),
+                        mail.getText(),
+                        birthday.getText(),
+                        gender,
+                        countryShortName,
+                        timeZone.getText()
+                );
+
+                if (!errors.isEmpty()) {
+                    return ErrorEncounteredJson.toJson(errors);
+                }
+
+                RegisterUser registerUser = new RegisterUser()
+                        .setQueueUserId(mail.getText())
+                        .setFirstName(new ScrubbedInput(firstName))
+                        .setLastName(new ScrubbedInput(lastName))
+                        .setAddress(address)
+                        .setAddressOrigin(AddressOriginEnum.S)
+                        .setBirthday(birthday)
+                        .setGender(GenderEnum.valueOf(gender))
+                        .setCountryShortName(new ScrubbedInput(countryShortName))
+                        .setTimeZone(timeZone)
+                        .setPhone(new ScrubbedInput(phone));
+                accountService.updateUserProfile(registerUser, mail.getText());
+            }
+
+            UserAccountEntity userAccount = accountService.findByQueueUserId(qid);
+            UserProfileEntity userProfile = userProfilePreferenceService.findByQueueUserId(userAccount.getQueueUserId());
+            int remoteJoin = inviteService.getRemoteJoinCount(userAccount.getQueueUserId());
+            LOG.info("Remote join available={}", remoteJoin);
+
+            return JsonProfile.newInstance(userProfile, remoteJoin).asJson();
+        } catch(Exception e) {
+            LOG.error("Failed getting profile qid={}, reason={}", qid, e.getLocalizedMessage(), e);
+            methodStatusSuccess = false;
+            return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+        } finally {
+            apiHealthService.insert(
+                    "/update",
+                    "update",
+                    ClientProfileAPIController.class.getName(),
+                    Duration.between(start, Instant.now()),
+                    methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
+        }
     }
 
     @PostMapping(
@@ -282,6 +376,16 @@ public class ClientProfileAPIController {
         Set<String> keys = new HashSet<>(map.keySet());
         List<AccountMobileService.ACCOUNT_MIGRATE> enums = new ArrayList<>(Arrays.asList(AccountMobileService.ACCOUNT_MIGRATE.values()));
         for (AccountMobileService.ACCOUNT_MIGRATE registration : enums) {
+            keys.remove(registration.name());
+        }
+
+        return keys;
+    }
+
+    private Set<String> invalidElementsInMapDuringUpdate(Map<String, ScrubbedInput> map) {
+        Set<String> keys = new HashSet<>(map.keySet());
+        List<AccountMobileService.ACCOUNT_UPDATE> enums = new ArrayList<>(Arrays.asList(AccountMobileService.ACCOUNT_UPDATE.values()));
+        for (AccountMobileService.ACCOUNT_UPDATE registration : enums) {
             keys.remove(registration.name());
         }
 
