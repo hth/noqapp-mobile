9d9ed95f73fa93d6cd9136e62375b9a5a2bc6742
hth <hth@users.noreply.github.com>
2018-05-28 14:15:57 +0530

    fixed length issue in validation

--- view/src/main/java/com/noqapp/mobile/view/validator/AccountClientValidator.java
+++ view/src/main/java/com/noqapp/mobile/view/validator/AccountClientValidator.java
@@ -132,7 +132,7 @@ public class AccountClientValidator {
     }
 
     void firstNameValidation(String firstName, Map<String, String> errors) {
-        if (StringUtils.isBlank(firstName) || firstName.length() <= nameLength) {
+        if (StringUtils.isBlank(firstName) || firstName.length() < nameLength) {
             LOG.info("failed validation firstName={}", firstName);
             errors.put(ErrorEncounteredJson.REASON, "Name validation failed. Minimum length '" + nameLength + "' characters");
             errors.put(AccountMobileService.ACCOUNT_REGISTRATION.FN.name(), StringUtils.isBlank(firstName) ? EMPTY : firstName);
@@ -142,7 +142,7 @@ public class AccountClientValidator {
     }
 
     void mailValidation(String mail, Map<String, String> errors) {
-        if (StringUtils.isBlank(mail) || mail.length() <= mailLength) {
+        if (StringUtils.isBlank(mail) || mail.length() < mailLength) {
             LOG.info("failed validation mail={}", mail);
             errors.put(ErrorEncounteredJson.REASON, "Mail validation failed. Minimum length '" + mailLength + "' characters");
             errors.put(AccountMobileService.ACCOUNT_REGISTRATION.EM.name(), StringUtils.isBlank(mail) ? EMPTY : mail);
