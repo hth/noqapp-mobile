4a5f45905a529ba3fd10ab5700c9ec289492be78
hth <hth@users.noreply.github.com>
2018-05-31 17:29:20 +0530

    removed unused import

--- view/src/main/java/com/noqapp/mobile/view/controller/open/HealthCareProfileController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/open/HealthCareProfileController.java
@@ -5,7 +5,6 @@ import com.noqapp.domain.json.JsonResponse;
 import com.noqapp.health.domain.types.HealthStatusEnum;
 import com.noqapp.health.service.ApiHealthService;
 import com.noqapp.medical.service.HealthCareProfileService;
-import com.noqapp.mobile.view.controller.api.client.ClientProfileAPIController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 import java.time.Duration;
 import java.time.Instant;
 
