7485ae8e7a1d17f0029b799077b952303d263112
hth <hth@users.noreply.github.com>
2018-06-29 11:22:41 +0530

    added condition to prevent adding of medical record by un-authorized business and user

--- view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/health/MedicalRecordController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/health/MedicalRecordController.java
@@ -1,17 +1,21 @@
 package com.noqapp.mobile.view.controller.api.merchant.health;
 
+import static com.noqapp.common.utils.CommonUtil.AUTH_KEY_HIDDEN;
+import static com.noqapp.common.utils.CommonUtil.UNAUTHORIZED;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.BUSINESS_NOT_AUTHORIZED;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MEDICAL_RECORD_ENTRY_DENIED;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MOBILE_JSON;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.SEVERE;
+import static com.noqapp.mobile.view.controller.open.DeviceController.getErrorReason;
+
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.noqapp.common.utils.ParseJsonStringToMap;
-import com.noqapp.common.utils.ScrubbedInput;
-import com.noqapp.domain.json.JsonResponse;
-import com.noqapp.health.domain.types.HealthStatusEnum;
-import com.noqapp.health.service.ApiHealthService;
-import com.noqapp.medical.domain.json.JsonMedicalRecord;
-import com.noqapp.medical.service.MedicalRecordService;
-import com.noqapp.mobile.service.AuthenticateMobileService;
+
+import org.apache.commons.lang3.StringUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,16 +24,24 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
+import com.noqapp.common.utils.ScrubbedInput;
+import com.noqapp.domain.BizStoreEntity;
+import com.noqapp.domain.json.JsonResponse;
+import com.noqapp.domain.types.BusinessTypeEnum;
+import com.noqapp.domain.types.UserLevelEnum;
+import com.noqapp.health.domain.types.HealthStatusEnum;
+import com.noqapp.health.service.ApiHealthService;
+import com.noqapp.medical.domain.json.JsonMedicalRecord;
+import com.noqapp.medical.service.MedicalRecordService;
+import com.noqapp.mobile.service.AuthenticateMobileService;
+import com.noqapp.service.BizService;
+import com.noqapp.service.BusinessUserStoreService;
+
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Map;
 
-import static com.noqapp.common.utils.CommonUtil.AUTH_KEY_HIDDEN;
-import static com.noqapp.common.utils.CommonUtil.UNAUTHORIZED;
-import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.SEVERE;
-import static com.noqapp.mobile.view.controller.open.DeviceController.getErrorReason;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * hitender
@@ -49,16 +61,22 @@ public class MedicalRecordController {
     private AuthenticateMobileService authenticateMobileService;
     private ApiHealthService apiHealthService;
     private MedicalRecordService medicalRecordService;
+    private BusinessUserStoreService businessUserStoreService;
+    private BizService bizService;
 
     @Autowired
     public MedicalRecordController(
             AuthenticateMobileService authenticateMobileService,
             ApiHealthService apiHealthService,
-            MedicalRecordService medicalRecordService
+            MedicalRecordService medicalRecordService,
+            BusinessUserStoreService businessUserStoreService,
+            BizService bizService
     ) {
         this.authenticateMobileService = authenticateMobileService;
         this.apiHealthService = apiHealthService;
         this.medicalRecordService = medicalRecordService;
+        this.businessUserStoreService = businessUserStoreService;
+        this.bizService = bizService;
     }
 
     /**
@@ -101,8 +119,28 @@ public class MedicalRecordController {
         try {
             JsonMedicalRecord jsonMedicalRecord = new ObjectMapper().readValue(requestBodyJson, JsonMedicalRecord.class);
             jsonMedicalRecord.setDiagnosedById(qid);
-            medicalRecordService.addMedicalRecord(jsonMedicalRecord);
 
+            if (StringUtils.isBlank(jsonMedicalRecord.getCodeQR())) {
+                LOG.warn("Not a valid codeQR={} qid={}", jsonMedicalRecord.getCodeQR(), qid);
+                return getErrorReason("Not a valid queue code.", MOBILE_JSON);
+            } else if (!businessUserStoreService.hasAccessWithUserLevel(
+                    jsonMedicalRecord.getDiagnosedById(),
+                    jsonMedicalRecord.getCodeQR(),
+                    UserLevelEnum.S_MANAGER)) {
+                LOG.info("Your are not authorized to add medical record mail={}", mail);
+                return getErrorReason("Your are not authorized to add medical record", MEDICAL_RECORD_ENTRY_DENIED);
+            }
+
+            /* Check if business type is of Hospital or Doctor to allow adding record. */
+            BizStoreEntity bizStore = bizService.findByCodeQR(jsonMedicalRecord.getCodeQR());
+            if (bizStore.getBusinessType() != BusinessTypeEnum.DO && bizStore.getBizName().getBusinessType() != BusinessTypeEnum.DO) {
+                LOG.error("Failed as its not a Doctor or Hospital business type, found store={} biz={}",
+                        bizStore.getBusinessType(),
+                        bizStore.getBizName().getBusinessType());
+                return getErrorReason("Business not authorized to add medical record", BUSINESS_NOT_AUTHORIZED);
+            }
+
+            medicalRecordService.addMedicalRecord(jsonMedicalRecord);
             return new JsonResponse(true).asJson();
         } catch (JsonMappingException e) {
             LOG.error("Failed parsing json={} qid={} message={}", requestBodyJson, qid, e.getLocalizedMessage(), e);
