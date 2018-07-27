94bc4764c283b7763234351df45a3d3d06fa67d5
hth <hth@users.noreply.github.com>
2018-07-27 15:54:15 +0530

    added API to show order

---
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/order/PurchaseOrderController.java
@@ -0,0 +1,136 @@
+package com.noqapp.mobile.view.controller.api.merchant.order;
+
+import static com.noqapp.common.utils.CommonUtil.AUTH_KEY_HIDDEN;
+import static com.noqapp.common.utils.CommonUtil.UNAUTHORIZED;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MOBILE_JSON;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.SEVERE;
+import static com.noqapp.mobile.view.controller.open.DeviceController.getErrorReason;
+
+import com.noqapp.common.utils.ScrubbedInput;
+import com.noqapp.domain.TokenQueueEntity;
+import com.noqapp.domain.json.JsonTopic;
+import com.noqapp.health.domain.types.HealthStatusEnum;
+import com.noqapp.health.service.ApiHealthService;
+import com.noqapp.mobile.service.AuthenticateMobileService;
+import com.noqapp.mobile.view.controller.api.merchant.ManageQueueController;
+import com.noqapp.service.BusinessUserStoreService;
+import com.noqapp.service.PurchaseOrderService;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * hitender
+ * 7/27/18 3:13 PM
+ */
+@SuppressWarnings ({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LongVariable"
+})
+@RestController
+@RequestMapping(value = "/api/m/o/purchaseOrder")
+public class PurchaseOrderController {
+    private static final Logger LOG = LoggerFactory.getLogger(PurchaseOrderController.class);
+
+    private AuthenticateMobileService authenticateMobileService;
+    private BusinessUserStoreService businessUserStoreService;
+    private PurchaseOrderService purchaseOrderService;
+    private ApiHealthService apiHealthService;
+
+    @Autowired
+    public PurchaseOrderController(
+        AuthenticateMobileService authenticateMobileService,
+        BusinessUserStoreService businessUserStoreService,
+        PurchaseOrderService purchaseOrderService,
+        ApiHealthService apiHealthService
+    ) {
+        this.authenticateMobileService = authenticateMobileService;
+        this.businessUserStoreService = businessUserStoreService;
+        this.purchaseOrderService = purchaseOrderService;
+        this.apiHealthService = apiHealthService;
+    }
+
+    /**
+     * List all clients.
+     */
+    @PostMapping(
+        value = "/showOrders/{codeQR}",
+        produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
+    )
+    public String showOrders(
+        @RequestHeader ("X-R-DID")
+        ScrubbedInput did,
+
+        @RequestHeader ("X-R-DT")
+        ScrubbedInput deviceType,
+
+        @RequestHeader ("X-R-MAIL")
+        ScrubbedInput mail,
+
+        @RequestHeader ("X-R-AUTH")
+        ScrubbedInput auth,
+
+        @PathVariable ("codeQR")
+        ScrubbedInput codeQR,
+
+        HttpServletResponse response
+    ) throws IOException {
+        boolean methodStatusSuccess = true;
+        Instant start = Instant.now();
+        LOG.info("Clients shown for codeQR={} request from mail={} did={} deviceType={} auth={}",
+            codeQR,
+            mail,
+            did,
+            deviceType,
+            AUTH_KEY_HIDDEN);
+
+        String qid = authenticateMobileService.getQueueUserId(mail.getText(), auth.getText());
+        if (null == qid) {
+            LOG.warn("Un-authorized access to /api/m/o/purchaseOrder/showOrders by mail={}", mail);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED);
+            return null;
+        }
+
+        if (StringUtils.isBlank(codeQR.getText())) {
+            LOG.warn("Not a valid codeQR={} qid={}", codeQR.getText(), qid);
+            return getErrorReason("Not a valid queue code.", MOBILE_JSON);
+        } else if (!businessUserStoreService.hasAccess(qid, codeQR.getText())) {
+            LOG.info("Un-authorized store access to /api/m/o/purchaseOrder/showOrders by mail={}", mail);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED);
+            return null;
+        }
+
+        try {
+            return purchaseOrderService.findAllOpenOrderByCodeAsJson(codeQR.getText());
+        } catch (Exception e) {
+            LOG.error("Failed getting order clients reason={}", e.getLocalizedMessage(), e);
+            methodStatusSuccess = false;
+            return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+        } finally {
+            apiHealthService.insert(
+                "/showOrders/{codeQR}",
+                "showOrders",
+                PurchaseOrderController.class.getName(),
+                Duration.between(start, Instant.now()),
+                methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
+        }
+    }
+}
