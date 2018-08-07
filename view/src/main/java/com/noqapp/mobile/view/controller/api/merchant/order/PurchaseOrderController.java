9eab294d4680cbafbdb72a86ef35564f6880f9d8
hth <hth@users.noreply.github.com>
2018-08-07 15:12:24 +0530

    starting of purchase order and changed in FCM messages

--- view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/order/PurchaseOrderController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/order/PurchaseOrderController.java
@@ -2,16 +2,28 @@ package com.noqapp.mobile.view.controller.api.merchant.order;
 
 import static com.noqapp.common.utils.CommonUtil.AUTH_KEY_HIDDEN;
 import static com.noqapp.common.utils.CommonUtil.UNAUTHORIZED;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MERCHANT_COULD_NOT_ACQUIRE;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MOBILE;
 import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MOBILE_JSON;
 import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.SEVERE;
 import static com.noqapp.mobile.view.controller.open.DeviceController.getErrorReason;
 
+import com.noqapp.common.utils.ParseJsonStringToMap;
 import com.noqapp.common.utils.ScrubbedInput;
+import com.noqapp.domain.TokenQueueEntity;
+import com.noqapp.domain.json.JsonToken;
+import com.noqapp.domain.types.QueueStatusEnum;
+import com.noqapp.domain.types.QueueUserStateEnum;
+import com.noqapp.domain.types.TokenServiceEnum;
 import com.noqapp.health.domain.types.HealthStatusEnum;
 import com.noqapp.health.service.ApiHealthService;
 import com.noqapp.mobile.service.AuthenticateMobileService;
+import com.noqapp.mobile.service.QueueMobileService;
+import com.noqapp.mobile.view.controller.api.merchant.ManageQueueController;
 import com.noqapp.service.BusinessUserStoreService;
 import com.noqapp.service.PurchaseOrderService;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -19,9 +31,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +44,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -47,27 +63,33 @@ import javax.servlet.http.HttpServletResponse;
 public class PurchaseOrderController {
     private static final Logger LOG = LoggerFactory.getLogger(PurchaseOrderController.class);
 
+    private int counterNameLength;
     private AuthenticateMobileService authenticateMobileService;
     private BusinessUserStoreService businessUserStoreService;
     private PurchaseOrderService purchaseOrderService;
+    private QueueMobileService queueMobileService;
     private ApiHealthService apiHealthService;
 
     @Autowired
     public PurchaseOrderController(
+        @Value("${ManageQueueController.counterNameLength}")
+        int counterNameLength,
+
         AuthenticateMobileService authenticateMobileService,
         BusinessUserStoreService businessUserStoreService,
         PurchaseOrderService purchaseOrderService,
+        QueueMobileService queueMobileService,
         ApiHealthService apiHealthService
     ) {
+        this.counterNameLength = counterNameLength;
         this.authenticateMobileService = authenticateMobileService;
         this.businessUserStoreService = businessUserStoreService;
         this.purchaseOrderService = purchaseOrderService;
+        this.queueMobileService = queueMobileService;
         this.apiHealthService = apiHealthService;
     }
 
-    /**
-     * List all clients.
-     */
+    /** List all orders. */
     @PostMapping(
         value = "/showOrders/{codeQR}",
         produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
@@ -126,6 +148,244 @@ public class PurchaseOrderController {
                 "/showOrders/{codeQR}",
                 "showOrders",
                 PurchaseOrderController.class.getName(),
+                Duration.between(start, Instant.now()),
+                methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
+        }
+    }
+
+//    /**
+//     * When client is served by merchant.
+//     * And
+//     * When client starts to serve for first time or re-start after serving the last in the queue.
+//     */
+//    @PostMapping(
+//        value = "/served",
+//        produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
+//    )
+//    public String served(
+//        @RequestHeader ("X-R-DID")
+//            ScrubbedInput did,
+//
+//        @RequestHeader ("X-R-DT")
+//            ScrubbedInput deviceType,
+//
+//        @RequestHeader ("X-R-MAIL")
+//            ScrubbedInput mail,
+//
+//        @RequestHeader ("X-R-AUTH")
+//            ScrubbedInput auth,
+//
+//        @RequestBody
+//            String requestBodyJson,
+//
+//        HttpServletResponse response
+//    ) throws IOException {
+//        boolean methodStatusSuccess = true;
+//        Instant start = Instant.now();
+//        LOG.info("Served mail={} did={} deviceType={} auth={}", mail, did, deviceType, AUTH_KEY_HIDDEN);
+//        String qid = authenticateMobileService.getQueueUserId(mail.getText(), auth.getText());
+//        if (null == qid) {
+//            LOG.warn("Un-authorized access to /api/m/mq/served by mail={}", mail);
+//            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED);
+//            return null;
+//        }
+//
+//        try {
+//            Map<String, ScrubbedInput> map = ParseJsonStringToMap.jsonStringToMap(requestBodyJson);
+//            String codeQR = map.containsKey("qr") ? map.get("qr").getText() : null;
+//
+//            if (StringUtils.isBlank(codeQR)) {
+//                LOG.warn("Not a valid codeQR={} qid={}", codeQR, qid);
+//                return getErrorReason("Not a valid queue code.", MOBILE_JSON);
+//            } else if (!businessUserStoreService.hasAccess(qid, codeQR)) {
+//                LOG.info("Un-authorized store access to /api/m/mq/served by mail={}", mail);
+//                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED);
+//                return null;
+//            }
+//
+//            String serveTokenString = map.containsKey("t") ? map.get("t").getText() : null;
+//            int servedNumber;
+//            if (StringUtils.isNumeric(serveTokenString)) {
+//                servedNumber = Integer.parseInt(serveTokenString);
+//            } else {
+//                LOG.warn("Not a valid number={} codeQR={} qid={}", serveTokenString, codeQR, qid);
+//                return getErrorReason("Not a valid number.", MOBILE_JSON);
+//            }
+//
+//            QueueUserStateEnum queueUserState;
+//            try {
+//                queueUserState = map.containsKey("q") ? QueueUserStateEnum.valueOf(map.get("q").getText()) : null;
+//            } catch (IllegalArgumentException e) {
+//                LOG.error("Failed finding QueueUserState reason={}", e.getLocalizedMessage(), e);
+//                return getErrorReason("Not a valid queue user state.", MOBILE_JSON);
+//            }
+//
+//            QueueStatusEnum queueStatus;
+//            try {
+//                queueStatus = map.containsKey("s") ? QueueStatusEnum.valueOf(map.get("s").getText()) : null;
+//                Assert.notNull(queueStatus, "Queue Status cannot be null");
+//            } catch (IllegalArgumentException e) {
+//                LOG.error("Failed finding QueueStatus reason={}", e.getLocalizedMessage(), e);
+//                return getErrorReason("Not a valid queue status.", MOBILE_JSON);
+//            }
+//
+//            String goTo = map.containsKey("g") ? map.get("g").getText() : null;
+//            if (StringUtils.isBlank(goTo)) {
+//                return getErrorReason("Counter name cannot be empty.", MOBILE_JSON);
+//            } else {
+//                if (goTo.length() > counterNameLength) {
+//                    return getErrorReason("Counter name cannot exceed character size of 20.", MOBILE_JSON);
+//                }
+//            }
+//
+//            TokenQueueEntity tokenQueue = queueMobileService.getTokenQueueByCodeQR(codeQR);
+//            LOG.info("queueStatus received={} found={}", queueStatus, tokenQueue.getQueueStatus());
+//
+//            JsonToken jsonToken;
+//            switch (tokenQueue.getQueueStatus()) {
+//                case C:
+//                case D:
+//                case N:
+//                    jsonToken = purchaseOrderService.updateAndGetNextInQueue(codeQR, servedNumber, queueUserState, goTo, did.getText(), TokenServiceEnum.M);
+//                    break;
+//                case R:
+//                case S:
+//                    jsonToken = purchaseOrderService.getNextInQueue(codeQR, goTo, did.getText());
+//
+//                    /* Remove delay or any setting associated before starting of queue. */
+//                    LOG.info("Resetting queue when queue status={}", tokenQueue.getQueueStatus());
+//                    tokenQueueService.resetQueueSettingWhenQueueStarts(codeQR);
+//                    break;
+//                default:
+//                    LOG.error("Reached unsupported condition queueState={}", tokenQueue.getQueueStatus());
+//                    throw new UnsupportedOperationException("Reached unsupported condition for QueueState " + tokenQueue.getQueueStatus().getDescription());
+//            }
+//
+//            if (null == jsonToken) {
+//                LOG.error("Could not find queue codeQR={} servedNumber={} queueUserState={}", codeQR, servedNumber, queueUserState);
+//                return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+//            }
+//
+//            LOG.info("On served response servedNumber={} nowServicing={} jsonToken={}", servedNumber, jsonToken.getServingNumber(), jsonToken);
+//            return jsonToken.asJson();
+//        } catch (JsonMappingException e) {
+//            LOG.error("Failed parsing json={} qid={} message={}", requestBodyJson, qid, e.getLocalizedMessage(), e);
+//            methodStatusSuccess = false;
+//            return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+//        } finally {
+//            apiHealthService.insert(
+//                "/served",
+//                "served",
+//                ManageQueueController.class.getName(),
+//                Duration.between(start, Instant.now()),
+//                methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
+//        }
+//    }
+
+    /**
+     * Acquire specific token not in order. Send message of being served next to the owner of the token.
+     */
+    @PostMapping (
+        value = "/acquire",
+        produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
+    )
+    public String acquire(
+        @RequestHeader ("X-R-DID")
+        ScrubbedInput did,
+
+        @RequestHeader ("X-R-DT")
+        ScrubbedInput deviceType,
+
+        @RequestHeader ("X-R-MAIL")
+        ScrubbedInput mail,
+
+        @RequestHeader ("X-R-AUTH")
+        ScrubbedInput auth,
+
+        @RequestBody
+        String requestBodyJson,
+
+        HttpServletResponse response
+    ) throws IOException {
+        boolean methodStatusSuccess = true;
+        Instant start = Instant.now();
+        LOG.info("Acquired mail={} did={} deviceType={} auth={}", mail, did, deviceType, AUTH_KEY_HIDDEN);
+        String qid = authenticateMobileService.getQueueUserId(mail.getText(), auth.getText());
+        if (null == qid) {
+            LOG.warn("Un-authorized access to /api/m/o/purchaseOrder by mail={}", mail);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED);
+            return null;
+        }
+
+        try {
+            Map<String, ScrubbedInput> map = ParseJsonStringToMap.jsonStringToMap(requestBodyJson);
+            String codeQR = map.containsKey("qr") ? map.get("qr").getText() : null;
+
+            if (StringUtils.isBlank(codeQR)) {
+                LOG.warn("Not a valid codeQR={} qid={}", codeQR, qid);
+                return getErrorReason("Not a valid queue code.", MOBILE_JSON);
+            } else if (!businessUserStoreService.hasAccess(qid, codeQR)) {
+                LOG.info("Un-authorized store access to /api/m/o/purchaseOrder/acquire by mail={}", mail);
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED);
+                return null;
+            }
+
+            String serveTokenString = map.containsKey("t") ? map.get("t").getText() : null;
+            int servedNumber;
+            if (StringUtils.isNumeric(serveTokenString)) {
+                servedNumber = Integer.parseInt(serveTokenString);
+            } else {
+                LOG.warn("Not a valid number={} codeQR={} qid={}", serveTokenString, codeQR, qid);
+                return getErrorReason("Not a valid number.", MOBILE_JSON);
+            }
+
+            QueueStatusEnum queueStatus;
+            try {
+                queueStatus = map.containsKey("s") ? QueueStatusEnum.valueOf(map.get("s").getText()) : null;
+                Assert.notNull(queueStatus, "Queue Status cannot be null");
+            } catch (IllegalArgumentException e) {
+                LOG.error("Failed finding QueueStatus reason={}", e.getLocalizedMessage(), e);
+                return getErrorReason("Not a valid queue status.", MOBILE_JSON);
+            }
+
+            String goTo = map.containsKey("g") ? map.get("g").getText() : "";
+            if (StringUtils.isBlank(goTo)) {
+                return getErrorReason("Counter name cannot be empty.", MOBILE_JSON);
+            } else {
+                if (counterNameLength < goTo.length()) {
+                    return getErrorReason("Counter name cannot exceed character size of 20.", MOBILE_JSON);
+                }
+            }
+
+            TokenQueueEntity tokenQueue = queueMobileService.getTokenQueueByCodeQR(codeQR);
+            LOG.info("queueStatus received={} found={}, supports only queueState=Next", queueStatus, tokenQueue.getQueueStatus());
+
+            JsonToken jsonToken;
+            switch (tokenQueue.getQueueStatus()) {
+                case N:
+                    jsonToken = purchaseOrderService.getThisAsNextInQueue(codeQR, goTo, did.getText(), servedNumber);
+                    break;
+                default:
+                    //TODO(hth) remind apps to call state of the queue when failure is encountered as state might have changed. Update app with this state.
+                    LOG.warn("Un-supported condition reached for acquiring token={} when queueState={}, supports only queueState=Next", servedNumber, tokenQueue.getQueueStatus());
+                    throw new UnsupportedOperationException("Reached unsupported condition for QueueState " + tokenQueue.getQueueStatus().getDescription());
+            }
+
+            if (null == jsonToken) {
+                LOG.warn("Failed to acquire client={} qid={} did={}", serveTokenString, qid, did);
+                return getErrorReason("Could not acquire client " + serveTokenString, MERCHANT_COULD_NOT_ACQUIRE);
+            }
+            LOG.info("On served response servedNumber={} nowServicing={} jsonToken={}", servedNumber, jsonToken.getServingNumber(), jsonToken);
+            return jsonToken.asJson();
+        } catch (JsonMappingException e) {
+            LOG.error("Failed parsing json={} qid={} message={}", requestBodyJson, qid, e.getLocalizedMessage(), e);
+            methodStatusSuccess = false;
+            return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+        } finally {
+            apiHealthService.insert(
+                "/acquire",
+                "acquire",
+                ManageQueueController.class.getName(),
                 Duration.between(start, Instant.now()),
                 methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
         }
