5f16467c1b439cbf72e2362556d57a7cedcca5b7
hth <hth@users.noreply.github.com>
2018-09-12 13:06:57 +0530

    merged two apis

--- view/src/main/java/com/noqapp/mobile/view/controller/open/DeviceController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/open/DeviceController.java
@@ -133,7 +133,7 @@ public class DeviceController {
 
     /** Checks is device version is supported. */
     @PostMapping (
-        value = "/version",
+        value = {"/version", "/v1/version"},
         produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
     )
     public String isSupportedAppVersion(
@@ -186,65 +186,6 @@ public class DeviceController {
                 DeviceController.class.getName(),
                 Duration.between(start, Instant.now()),
                 methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
-        }
-    }
-
-    @Deprecated
-    /** Checks is device version is supported. */
-    @PostMapping (
-            value = "/v1/version",
-            produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
-    )
-    public String isSupportedAppVersionObsolete(
-            @RequestHeader ("X-R-DID")
-            ScrubbedInput did,
-
-            @RequestHeader ("X-R-DT")
-            ScrubbedInput deviceType,
-
-            @RequestHeader ("X-R-AF")
-            ScrubbedInput appFlavor,
-
-            @RequestHeader (value = "X-R-VR")
-            ScrubbedInput versionRelease
-    ) {
-        boolean methodStatusSuccess = true;
-        Instant start = Instant.now();
-        LOG.info("Supported device did={} deviceType={} appFlavor={} versionRelease={}", did, deviceType, appFlavor, versionRelease);
-
-        try {
-            DeviceTypeEnum deviceTypeEnum = DeviceTypeEnum.valueOf(deviceType.getText());
-            AppFlavorEnum appFlavorEnum = AppFlavorEnum.valueOf(appFlavor.getText());
-            LOG.info("Check if API version is supported for {} versionRelease={}",
-                    deviceTypeEnum.getDescription(),
-                    versionRelease);
-
-            try {
-                LowestSupportedAppEnum lowestSupportedApp = LowestSupportedAppEnum.findBasedOnDeviceType(deviceTypeEnum, appFlavorEnum);
-                if (!LowestSupportedAppEnum.isSupportedVersion(lowestSupportedApp, versionRelease.getText())) {
-                    LOG.warn("Sent warning to upgrade versionNumber={}", versionRelease.getText());
-                    return getErrorReason("To continue, please upgrade to latest version", MOBILE_UPGRADE);
-                }
-
-                return new JsonLatestAppVersion(lowestSupportedApp.getLatestAppVersion()).asJson();
-            } catch (NumberFormatException e) {
-                LOG.error("Failed parsing API version, reason={}", e.getLocalizedMessage(), e);
-                return getErrorReason("Failed to read API version type.", USER_INPUT);
-            } catch (Exception e) {
-                LOG.error("Failed parsing API version, reason={}", e.getLocalizedMessage(), e);
-                return getErrorReason("Incorrect API version type.", USER_INPUT);
-            }
-        } catch (Exception e) {
-            LOG.error("Failed parsing deviceType, reason={}", e.getLocalizedMessage(), e);
-            methodStatusSuccess = false;
-            return getErrorReason("Incorrect device type.", USER_INPUT);
-        } finally {
-            apiHealthService.insert(
-                    "/v1/version",
-                    "/v1/version",
-                    DeviceController.class.getName(),
-                    Duration.between(start, Instant.now()),
-                    methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
         }
     }
 
