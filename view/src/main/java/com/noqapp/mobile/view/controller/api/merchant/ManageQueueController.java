559e6610071df29243a24a9cc871cf81f7033c75
hth <hth@users.noreply.github.com>
2018-09-17 15:30:27 +0530

    optimized import

--- view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/ManageQueueController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/ManageQueueController.java
@@ -65,17 +65,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TimeZone;
 
 import javax.servlet.http.HttpServletResponse;
 
