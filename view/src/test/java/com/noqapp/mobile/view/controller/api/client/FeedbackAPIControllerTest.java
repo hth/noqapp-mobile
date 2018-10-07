3a619385eccd46519015d9c3350598509b0486fd
hth <hth@users.noreply.github.com>
2018-10-07 16:54:31 +0530

    added purchase order

--- view/src/test/java/com/noqapp/mobile/view/controller/api/client/FeedbackAPIControllerTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * User: hitender
- * Date: 10/6/18 9:39 PM
- */
-class FeedbackAPIControllerTest {
-
-}
--- view/src/test/java/com/noqapp/mobile/view/controller/api/client/PurchaseOrderAPIControllerTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * User: hitender
- * Date: 10/6/18 9:38 PM
- */
-class PurchaseOrderAPIControllerTest {
-
-}
