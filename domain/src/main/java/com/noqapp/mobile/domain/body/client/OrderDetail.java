1f36122953d69b49ccdbf7086a405e4d7d933f58
hth <hth@users.noreply.github.com>
2018-10-10 18:53:13 +0530

    API order detail

---
+++ domain/src/main/java/com/noqapp/mobile/domain/body/client/OrderDetail.java
@@ -0,0 +1,53 @@
+package com.noqapp.mobile.domain.body.client;
+
+import com.noqapp.common.utils.AbstractDomain;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+/**
+ * hitender
+ * 10/10/18 4:51 PM
+ */
+@SuppressWarnings({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LongVariable",
+    "unused"
+})
+@JsonAutoDetect(
+    fieldVisibility = JsonAutoDetect.Visibility.ANY,
+    getterVisibility = JsonAutoDetect.Visibility.NONE,
+    setterVisibility = JsonAutoDetect.Visibility.NONE
+)
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OrderDetail extends AbstractDomain {
+
+    @JsonProperty("qr")
+    private String codeQR;
+
+    @JsonProperty ("t")
+    private int token;
+
+    public String getCodeQR() {
+        return codeQR;
+    }
+
+    public OrderDetail setCodeQR(String codeQR) {
+        this.codeQR = codeQR;
+        return this;
+    }
+
+    public int getToken() {
+        return token;
+    }
+
+    public OrderDetail setToken(int token) {
+        this.token = token;
+        return this;
+    }
+}
