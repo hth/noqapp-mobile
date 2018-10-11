869f339f1c1c0e50fa78693f5af7ce70a9b139ef
hth <hth@users.noreply.github.com>
2018-10-11 14:28:13 +0530

    changed to use existing object

--- common/src/main/java/com/noqapp/mobile/common/util/MobileSystemErrorCodeEnum.java
+++ common/src/main/java/com/noqapp/mobile/common/util/MobileSystemErrorCodeEnum.java
@@ -42,6 +42,7 @@ public enum MobileSystemErrorCodeEnum {
 
     /** Orders. */
     PURCHASE_ORDER_NOT_FOUND("4204", ALERT),
+    PURCHASE_ORDER_FAILED_TO_CANCEL("4208", ALERT),
 
     /** Mobile application related issue. */
     SEVERE("5000", ERROR),
