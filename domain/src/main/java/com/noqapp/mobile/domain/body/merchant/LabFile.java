e01d09016073dece11905cc959e5914841b18c70
hth <hth@users.noreply.github.com>
2019-02-15 08:13:43 +0530

    added show attachment

---
+++ domain/src/main/java/com/noqapp/mobile/domain/body/merchant/LabFile.java
@@ -0,0 +1,80 @@
+package com.noqapp.mobile.domain.body.merchant;
+
+import com.noqapp.common.utils.AbstractDomain;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * hitender
+ * 2019-02-14 16:51
+ */
+@SuppressWarnings({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LongVariable",
+    "unused"
+})
+@JsonAutoDetect(
+    fieldVisibility = JsonAutoDetect.Visibility.ANY,
+    getterVisibility = JsonAutoDetect.Visibility.NONE,
+    setterVisibility = JsonAutoDetect.Visibility.NONE
+)
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LabFile extends AbstractDomain {
+
+    @JsonProperty("ti")
+    private String transactionId;
+
+    @JsonProperty("da")
+    private String deleteAttachment;
+
+    @JsonProperty("files")
+    private List<String> files = new LinkedList<>();
+
+    @JsonProperty("rr")
+    private String recordReferenceId;
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public LabFile setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+        return this;
+    }
+
+    public String getDeleteAttachment() {
+        return deleteAttachment;
+    }
+
+    public LabFile setDeleteAttachment(String deleteAttachment) {
+        this.deleteAttachment = deleteAttachment;
+        return this;
+    }
+
+    public List<String> getFiles() {
+        return files;
+    }
+
+    public LabFile setFiles(List<String> files) {
+        this.files = files;
+        return this;
+    }
+
+    public String getRecordReferenceId() {
+        return recordReferenceId;
+    }
+
+    public LabFile setRecordReferenceId(String recordReferenceId) {
+        this.recordReferenceId = recordReferenceId;
+        return this;
+    }
+}
