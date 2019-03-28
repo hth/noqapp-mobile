7f66926d0ab9c2370ce8fec9cee9f6e898738ee7
hth <hth@users.noreply.github.com>
2019-03-28 11:41:25 +0530

    Added log

--- view/src/main/java/com/noqapp/mobile/view/controller/api/client/TokenQueueAPIController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/client/TokenQueueAPIController.java
@@ -551,12 +551,15 @@ public class TokenQueueAPIController {
 
         try {
             LOG.info("codeQR={} qid={} guardianQid={}", joinQueue.getCodeQR(), joinQueue.getQueueUserId(), joinQueue.getGuardianQid());
-            return tokenQueueMobileService.skipPayBeforeJoinQueue(
+            String jsonTokenResponse = tokenQueueMobileService.skipPayBeforeJoinQueue(
                 joinQueue.getCodeQR(),
                 did.getText(),
                 joinQueue.getQueueUserId(),
                 joinQueue.getGuardianQid(),
                 bizStore).asJson();
+
+            LOG.debug("Skip Pay Before Join Response={}", jsonTokenResponse);
+            return jsonTokenResponse;
         } catch (Exception e) {
             LOG.error("Failed joining payBeforeQueue qid={}, reason={}", qid, e.getLocalizedMessage(), e);
             apiHealthService.insert(
