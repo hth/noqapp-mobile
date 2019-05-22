799f32c301e11dd825e41c7e8e1ee212ec196091
hth <hth@users.noreply.github.com>
2019-05-22 11:07:43 +0530

    Starting with support appointments

---
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/client/AppointmentController.java
@@ -0,0 +1,121 @@
+package com.noqapp.mobile.view.controller.api.client;
+
+import static com.noqapp.common.utils.CommonUtil.AUTH_KEY_HIDDEN;
+import static com.noqapp.common.utils.CommonUtil.UNAUTHORIZED;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.SEVERE;
+import static com.noqapp.mobile.view.controller.open.DeviceController.getErrorReason;
+
+import com.noqapp.common.utils.ScrubbedInput;
+import com.noqapp.domain.json.JsonSchedule;
+import com.noqapp.domain.json.JsonScheduleList;
+import com.noqapp.health.domain.types.HealthStatusEnum;
+import com.noqapp.health.service.ApiHealthService;
+import com.noqapp.mobile.service.AuthenticateMobileService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * User: hitender
+ * Date: 2019-05-22 10:36
+ */
+@SuppressWarnings ({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LongVariable"
+})
+@RestController
+@RequestMapping(value = "/api/c/appointment")
+public class AppointmentController {
+    private static final Logger LOG = LoggerFactory.getLogger(AppointmentController.class);
+
+    private AuthenticateMobileService authenticateMobileService;
+    private ApiHealthService apiHealthService;
+
+    @Autowired
+    public AppointmentController(
+        AuthenticateMobileService authenticateMobileService,
+        ApiHealthService apiHealthService
+    ) {
+        this.authenticateMobileService = authenticateMobileService;
+        this.apiHealthService = apiHealthService;
+    }
+
+    @GetMapping(
+        value = "/showSchedule",
+        produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
+    )
+    public String showSchedule(
+        @RequestHeader("X-R-MAIL")
+        ScrubbedInput mail,
+
+        @RequestHeader ("X-R-AUTH")
+        ScrubbedInput auth,
+
+        HttpServletResponse response
+    ) throws IOException {
+        boolean methodStatusSuccess = true;
+        Instant start = Instant.now();
+        LOG.debug("Appointment mail={}, auth={}", mail, AUTH_KEY_HIDDEN);
+        String qid = authenticateMobileService.getQueueUserId(mail.getText(), auth.getText());
+        if (null == qid) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED);
+            return null;
+        }
+
+        try {
+            JsonSchedule jsonSchedule1 = new JsonSchedule()
+                .setDay("2019-05-25")
+                .setTotalAppointments(20);
+
+            JsonSchedule jsonSchedule2 = new JsonSchedule()
+                .setDay("2019-05-28")
+                .setTotalAppointments(19);
+
+            JsonSchedule jsonSchedule3 = new JsonSchedule()
+                .setDay("2019-05-30")
+                .setTotalAppointments(3);
+
+            JsonSchedule jsonSchedule4 = new JsonSchedule()
+                .setDay("2019-06-05")
+                .setTotalAppointments(8);
+
+            JsonSchedule jsonSchedule5 = new JsonSchedule()
+                .setDay("2019-06-15")
+                .setTotalAppointments(6);
+
+            JsonScheduleList jsonSchedules = new JsonScheduleList();
+            jsonSchedules.addJsonSchedule(jsonSchedule1);
+            jsonSchedules.addJsonSchedule(jsonSchedule2);
+            jsonSchedules.addJsonSchedule(jsonSchedule3);
+            jsonSchedules.addJsonSchedule(jsonSchedule4);
+            jsonSchedules.addJsonSchedule(jsonSchedule5);
+            return jsonSchedules.asJson();
+        } catch (Exception e) {
+            LOG.error("Failed getting profile qid={}, reason={}", qid, e.getLocalizedMessage(), e);
+            methodStatusSuccess = false;
+            return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+        } finally {
+            apiHealthService.insert(
+                "/showSchedule",
+                "showSchedule",
+                AppointmentController.class.getName(),
+                Duration.between(start, Instant.now()),
+                methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
+        }
+    }
+}
