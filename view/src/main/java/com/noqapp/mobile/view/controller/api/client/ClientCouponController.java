86ddc33dbe44185256a565715f496428acd81518
hth <hth@users.noreply.github.com>
2019-06-13 23:45:32 +0530

    Added API to remove coupon

--- view/src/main/java/com/noqapp/mobile/view/controller/api/client/ClientCouponController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/client/ClientCouponController.java
@@ -3,23 +3,21 @@ package com.noqapp.mobile.view.controller.api.client;
 import static com.noqapp.common.utils.CommonUtil.AUTH_KEY_HIDDEN;
 import static com.noqapp.common.utils.CommonUtil.UNAUTHORIZED;
 import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.COUPON_NOT_APPLICABLE;
-import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.MOBILE_JSON;
-import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.PROMOTION_ACCESS_DENIED;
+import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.COUPON_REMOVAL_FAILED;
 import static com.noqapp.mobile.common.util.MobileSystemErrorCodeEnum.SEVERE;
 import static com.noqapp.mobile.view.controller.open.DeviceController.getErrorReason;
 
 import com.noqapp.common.utils.ScrubbedInput;
+import com.noqapp.domain.PurchaseOrderEntity;
 import com.noqapp.health.domain.types.HealthStatusEnum;
 import com.noqapp.health.service.ApiHealthService;
 import com.noqapp.mobile.domain.body.merchant.CouponOnOrder;
 import com.noqapp.mobile.service.AuthenticateMobileService;
-import com.noqapp.mobile.view.controller.api.merchant.MerchantCouponController;
-import com.noqapp.service.BusinessUserStoreService;
 import com.noqapp.service.CouponService;
+import com.noqapp.service.PurchaseOrderProductService;
 import com.noqapp.service.PurchaseOrderService;
 import com.noqapp.service.exceptions.CouponCannotApplyException;
-
-import org.apache.commons.lang3.StringUtils;
+import com.noqapp.service.exceptions.CouponRemovalException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,7 +25,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -57,6 +54,7 @@ public class ClientCouponController {
 
     private CouponService couponService;
     private PurchaseOrderService purchaseOrderService;
+    private PurchaseOrderProductService purchaseOrderProductService;
     private AuthenticateMobileService authenticateMobileService;
     private ApiHealthService apiHealthService;
 
@@ -64,11 +62,13 @@ public class ClientCouponController {
     public ClientCouponController(
         CouponService couponService,
         PurchaseOrderService purchaseOrderService,
+        PurchaseOrderProductService purchaseOrderProductService,
         AuthenticateMobileService authenticateMobileService,
         ApiHealthService apiHealthService
     ) {
         this.couponService = couponService;
         this.purchaseOrderService = purchaseOrderService;
+        this.purchaseOrderProductService = purchaseOrderProductService;
         this.authenticateMobileService = authenticateMobileService;
         this.apiHealthService = apiHealthService;
     }
@@ -153,7 +153,13 @@ public class ClientCouponController {
         }
 
         try {
-            return purchaseOrderService.applyCoupon(couponOnOrder.getQueueUserId(), couponOnOrder.getTransactionId(), couponOnOrder.getCouponId()).asJson();
+            PurchaseOrderEntity purchaseOrder = purchaseOrderService.applyCoupon(couponOnOrder.getQueueUserId(), couponOnOrder.getTransactionId(), couponOnOrder.getCouponId());
+            return purchaseOrderProductService.populateJsonPurchaseOrder(purchaseOrder).asJson();
+        } catch (CouponRemovalException e) {
+            LOG.error("Failed removing coupons for {} {} reason={}",
+                couponOnOrder.getCodeQR().getText(), couponOnOrder.getCouponId(), e.getLocalizedMessage(), e);
+            methodStatusSuccess = false;
+            return getErrorReason(e.getLocalizedMessage(), COUPON_REMOVAL_FAILED);
         } catch (CouponCannotApplyException e) {
             LOG.error("Failed applying coupons for {} {} reason={}",
                 couponOnOrder.getCodeQR().getText(), couponOnOrder.getCouponId(), e.getLocalizedMessage(), e);
@@ -168,6 +174,63 @@ public class ClientCouponController {
             apiHealthService.insert(
                 "/apply",
                 "apply",
+                ClientCouponController.class.getName(),
+                Duration.between(start, Instant.now()),
+                methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
+        }
+    }
+
+    @PostMapping(
+        value = "/remove",
+        headers = "Accept=" + MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
+    )
+    public String remove(
+        @RequestHeader("X-R-DID")
+        ScrubbedInput did,
+
+        @RequestHeader ("X-R-DT")
+        ScrubbedInput deviceType,
+
+        @RequestHeader("X-R-MAIL")
+        ScrubbedInput mail,
+
+        @RequestHeader ("X-R-AUTH")
+        ScrubbedInput auth,
+
+        @RequestBody
+        CouponOnOrder couponOnOrder,
+
+        HttpServletResponse response
+    ) throws IOException {
+        boolean methodStatusSuccess = true;
+        Instant start = Instant.now();
+        LOG.info("Remove coupon with mail={} did={} deviceType={} auth={}", mail, did, deviceType, AUTH_KEY_HIDDEN);
+        String qid = authenticateMobileService.getQueueUserId(mail.getText(), auth.getText());
+        if (null == qid) {
+            LOG.warn("Un-authorized access to /api/c/coupon/remove by mail={}", mail);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED);
+            return null;
+        }
+
+        try {
+            PurchaseOrderEntity purchaseOrder = purchaseOrderService.removeCoupon(couponOnOrder.getQueueUserId(), couponOnOrder.getTransactionId());
+            return purchaseOrderProductService.populateJsonPurchaseOrder(purchaseOrder).asJson();
+        } catch (CouponRemovalException e) {
+            LOG.error("Failed removing coupons for {} {} reason={}",
+                couponOnOrder.getCodeQR().getText(), couponOnOrder.getCouponId(), e.getLocalizedMessage(), e);
+            methodStatusSuccess = false;
+            return getErrorReason(e.getLocalizedMessage(), COUPON_REMOVAL_FAILED);
+        } catch (Exception e) {
+            LOG.error("Failed removing coupons for {} {} reason={}",
+                couponOnOrder.getCodeQR().getText(), couponOnOrder.getCouponId(), e.getLocalizedMessage(), e);
+            methodStatusSuccess = false;
+            return getErrorReason("Something went wrong. Engineers are looking into this.", SEVERE);
+        } finally {
+            apiHealthService.insert(
+                "/remove",
+                "remove",
                 ClientCouponController.class.getName(),
                 Duration.between(start, Instant.now()),
                 methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
