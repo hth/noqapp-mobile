a0cc91284f6796f37cf3b3f351ef54147c5b2681
hth <hth@users.noreply.github.com>
2019-06-21 14:40:32 +0530

    Added log (revert)

--- view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/queue/QueueController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/merchant/queue/QueueController.java
@@ -764,6 +764,7 @@ public class QueueController {
 
             UserProfileEntity userProfile = null;
             if (StringUtils.isNotBlank(businessCustomer.getCustomerPhone())) {
+                LOG.info("Look up customer by name {}", businessCustomer.getCustomerPhone());
                 userProfile = accountService.checkUserExistsByPhone(businessCustomer.getCustomerPhone());
                 if (!userProfile.getQueueUserId().equalsIgnoreCase(businessCustomer.getQueueUserId())) {
                     if (userProfile.getQidOfDependents().contains(businessCustomer.getQueueUserId())) {
