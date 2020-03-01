8455aa936f3701149fe0f052535900e746e10daf
hth <hth@users.noreply.github.com>
2020-03-01 05:00:34 -0800

    Added API for corona stats

---
+++ domain/src/main/java/com/noqapp/mobile/domain/JsonCoronaStatElement.java
@@ -0,0 +1,89 @@
+package com.noqapp.mobile.domain;
+
+import com.noqapp.common.utils.AbstractDomain;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+/**
+ * hitender
+ * 3/1/20 4:50 AM
+ */
+@SuppressWarnings ({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LongVariable",
+    "unused"
+})
+@JsonAutoDetect(
+    fieldVisibility = JsonAutoDetect.Visibility.ANY,
+    getterVisibility = JsonAutoDetect.Visibility.NONE,
+    setterVisibility = JsonAutoDetect.Visibility.NONE
+)
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JsonCoronaStatElement extends AbstractDomain {
+
+    String officialFS;
+    String officialSS;
+    String officialCC;
+    String trackedFS;
+    String trackedSS;
+    String trackedCC;
+
+    public String getOfficialFS() {
+        return officialFS;
+    }
+
+    public JsonCoronaStatElement setOfficialFS(String officialFS) {
+        this.officialFS = officialFS;
+        return this;
+    }
+
+    public String getOfficialSS() {
+        return officialSS;
+    }
+
+    public JsonCoronaStatElement setOfficialSS(String officialSS) {
+        this.officialSS = officialSS;
+        return this;
+    }
+
+    public String getOfficialCC() {
+        return officialCC;
+    }
+
+    public JsonCoronaStatElement setOfficialCC(String officialCC) {
+        this.officialCC = officialCC;
+        return this;
+    }
+
+    public String getTrackedFS() {
+        return trackedFS;
+    }
+
+    public JsonCoronaStatElement setTrackedFS(String trackedFS) {
+        this.trackedFS = trackedFS;
+        return this;
+    }
+
+    public String getTrackedSS() {
+        return trackedSS;
+    }
+
+    public JsonCoronaStatElement setTrackedSS(String trackedSS) {
+        this.trackedSS = trackedSS;
+        return this;
+    }
+
+    public String getTrackedCC() {
+        return trackedCC;
+    }
+
+    public JsonCoronaStatElement setTrackedCC(String trackedCC) {
+        this.trackedCC = trackedCC;
+        return this;
+    }
+}
