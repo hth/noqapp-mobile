e2c2224d6732a52c12bc9ab3f2ea84946b3809ff
hth <hth@users.noreply.github.com>
2020-05-14 03:12:16 -0700

    Update to latest version

--- domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
+++ domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
@@ -26,7 +26,7 @@ public enum LowestSupportedAppEnum {
     VACL("1.2.405",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                              //Device Type
         AppFlavorEnum.NQCL,
-        "1.2.405",                      //Latest App Version on Play Store
+        "1.2.408",                      //Latest App Version on Play Store
         "Version Android Client"),
 
     VACH("1.2.405",                     //Oldest Supported App Version in String
