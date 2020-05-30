12c5f28d0f7b50525035daa9756e60762c143dbe
hth <hth@users.noreply.github.com>
2020-05-30 00:03:48 -0700

    Force update

--- domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
+++ domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
@@ -23,16 +23,16 @@ public enum LowestSupportedAppEnum {
         "Version iPhone"),
     /* 1.1.10 is not released. */
 
-    VACL("1.2.447",                     //Oldest Supported App Version in String
+    VACL("1.2.449",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                              //Device Type
         AppFlavorEnum.NQCL,
-        "1.2.447",                      //Latest App Version on Play Store
+        "1.2.449",                      //Latest App Version on Play Store
         "Version Android Client"),
 
-    VACH("1.2.447",                     //Oldest Supported App Version in String
+    VACH("1.2.449",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                              //Device Type
         AppFlavorEnum.NQCH,
-        "1.2.447",                      //Latest App Version on Play Store
+        "1.2.449",                      //Latest App Version on Play Store
         "Version Android Business HealthCare"),
 
     VAMS("1.2.447",                     //Oldest Supported App Version in String
