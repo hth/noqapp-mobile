ccb77c6db03b26a8bea23328d3ef9dcaa0d39ec5
hth <hth@users.noreply.github.com>
2020-06-14 18:03:53 -0700

    Formatting

--- domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
+++ domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
@@ -17,44 +17,44 @@ public enum LowestSupportedAppEnum {
 
     /* List lowest supported version of iPhone and Android app. */
     VI("1.1.10",                        //Oldest Supported App Version in String
-        DeviceTypeEnum.I,                              //Device Type
+        DeviceTypeEnum.I,                               //Device Type
         AppFlavorEnum.NQCL,
         "1.1.10",                       //Latest App Version on App Store
         "Version iPhone"),
     /* 1.1.10 is not released. */
 
     VACL("1.2.458",                     //Oldest Supported App Version in String
-        DeviceTypeEnum.A,                              //Device Type
+        DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQCL,
         "1.2.488",                      //Latest App Version on Play Store
         "Version Android Client"),
 
     VACH("1.2.458",                     //Oldest Supported App Version in String
-        DeviceTypeEnum.A,                              //Device Type
+        DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQCH,
         "1.2.488",                      //Latest App Version on Play Store
         "Version Android Business HealthCare"),
 
     VAMS("1.2.458",                     //Oldest Supported App Version in String
-        DeviceTypeEnum.A,                              //Device Type
+        DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMS,
         "1.2.488",                      //Latest App Version on Play Store
         "Version Android Business Store"),
 
     VAMH("1.2.458",                     //Oldest Supported App Version in String
-        DeviceTypeEnum.A,                              //Device Type
+        DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMH,
         "1.2.488",                      //Latest App Version on Play Store
         "Version Android Business HealthCare"),
 
     VAMT("1.2.458",                     //Oldest Supported App Version in String
-        DeviceTypeEnum.A,                              //Device Type
+        DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMT,
         "1.2.488",                      //Latest App Version on Play Store
         "Version Android Business TV"),
 
     VAMI("1.2.458",                     //Oldest Supported App Version in String
-        DeviceTypeEnum.A,                              //Device Type
+        DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMI,
         "1.2.488",                      //Latest App Version on Play Store
         "Version Android Business Inventory");
