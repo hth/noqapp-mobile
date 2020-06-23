10d33fbf1696fb94e0b0496e50155bbdf5e5cd67
hth <hth@users.noreply.github.com>
2020-06-23 14:24:18 -0700

    Updated mongo configuration to latest available release

--- view/src/test/java/com/noqapp/mobile/view/RealMongoForITest.java
+++ view/src/test/java/com/noqapp/mobile/view/RealMongoForITest.java
@@ -1,7 +1,11 @@
 package com.noqapp.mobile.view;
 
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -16,6 +20,9 @@ import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * hitender
@@ -35,17 +42,44 @@ public abstract class RealMongoForITest extends ElasticForITest {
     private MongoTemplate mongoTemplate;
 
     private static final String DATABASE_NAME = "noqapp-i-test";
+    int port;
 
     @BeforeAll
     public void globalSetup() throws Exception {
-        int port = Network.getFreeServerPort();
-
+        port = Network.getFreeServerPort();
         mongodExecutable = starter.prepare(new MongodConfigBuilder()
                 .version(Version.Main.PRODUCTION)
                 .net(new Net("localhost", port, Network.localhostIsIPv6()))
                 .build());
         mongodProcess = mongodExecutable.start();
-        mongoClient = new MongoClient("localhost", port);
+        mongoClient = MongoClients.create(populateMongoClientSettings());
+    }
+
+    private MongoClientSettings populateMongoClientSettings() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+            .applicationName("NoQueue Test")
+            .applyToClusterSettings(builder -> builder.hosts(getMongoSeeds()))
+            .build();
+
+        return settings;
+    }
+
+    private List<ServerAddress> getMongoSeeds() {
+        List<ServerAddress> serverAddresses = new ArrayList<>();
+        String mongoReplicaSet = "localhost:" + port;
+
+        if (StringUtils.isNotBlank(mongoReplicaSet)) {
+            String[] mongoInternetAddresses = mongoReplicaSet.split(",");
+            for (String mongoInternetAddress : mongoInternetAddresses) {
+                String[] mongoIpAndPort = mongoInternetAddress.split(":");
+                ServerAddress serverAddress = new ServerAddress(mongoIpAndPort[0], Integer.parseInt(mongoIpAndPort[1]));
+                serverAddresses.add(serverAddress);
+            }
+        } else {
+            throw new RuntimeException("No Mongo Seed(s) listed");
+        }
+
+        return serverAddresses;
     }
 
     @AfterAll
@@ -58,7 +92,7 @@ public abstract class RealMongoForITest extends ElasticForITest {
         }
     }
 
-    public Mongo getMongo() {
+    public MongoClient getMongo() {
         return mongoClient;
     }
 
