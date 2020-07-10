e93c8d0d0e9cb80e798517fa2b076f717f71d80b
hth <hth@users.noreply.github.com>
2020-07-09 22:19:43 -0700

    Updated lowest support version

--- domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
+++ domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
@@ -35,25 +35,25 @@ public enum LowestSupportedAppEnum {
         "1.2.529",                      //Latest App Version on Play Store
         "Version Android Client HealthCare"),
 
-    VAMS("1.2.522",                     //Oldest Supported App Version in String
+    VAMS("1.2.529",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMS,
         "1.2.529",                      //Latest App Version on Play Store
         "Version Android Business Store"),
 
-    VAMH("1.2.522",                     //Oldest Supported App Version in String
+    VAMH("1.2.529",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMH,
         "1.2.529",                      //Latest App Version on Play Store
         "Version Android Business HealthCare"),
 
-    VAMT("1.2.522",                     //Oldest Supported App Version in String
+    VAMT("1.2.529",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMT,
         "1.2.529",                      //Latest App Version on Play Store
         "Version Android Business TV"),
 
-    VAMI("1.2.522",                     //Oldest Supported App Version in String
+    VAMI("1.2.529",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMI,
         "1.2.529",                      //Latest App Version on Play Store
