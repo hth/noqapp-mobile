fe8fffa4298a521257cc27e87229faf99e1ce02c
hth <hth@users.noreply.github.com>
2020-08-18 23:29:10 -0700

    Updated latest version

--- domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
+++ domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
@@ -26,37 +26,37 @@ public enum LowestSupportedAppEnum {
     VACL("1.2.562",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQCL,
-        "1.2.562",                      //Latest App Version on Play Store
+        "1.2.570",                      //Latest App Version on Play Store
         "Version Android Client"),
 
     VACH("1.2.562",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQCH,
-        "1.2.562",                      //Latest App Version on Play Store
+        "1.2.570",                      //Latest App Version on Play Store
         "Version Android Client HealthCare"),
 
     VAMS("1.2.562",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMS,
-        "1.2.562",                      //Latest App Version on Play Store
+        "1.2.570",                      //Latest App Version on Play Store
         "Version Android Business Store"),
 
     VAMH("1.2.562",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMH,
-        "1.2.562",                      //Latest App Version on Play Store
+        "1.2.570",                      //Latest App Version on Play Store
         "Version Android Business HealthCare"),
 
     VAMT("1.2.562",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMT,
-        "1.2.562",                      //Latest App Version on Play Store
+        "1.2.570",                      //Latest App Version on Play Store
         "Version Android Business TV"),
 
     VAMI("1.2.562",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,                               //Device Type
         AppFlavorEnum.NQMI,
-        "1.2.562",                      //Latest App Version on Play Store
+        "1.2.570",                      //Latest App Version on Play Store
         "Version Android Business Inventory");
 
     private static final Logger LOG = LoggerFactory.getLogger(LowestSupportedAppEnum.class);
