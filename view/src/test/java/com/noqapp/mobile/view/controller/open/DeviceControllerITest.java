4932f2c2912356b261f0e7b1d3903bdd3b50b7df
hth <hth@users.noreply.github.com>
2020-09-10 20:56:24 -0700

    fixed build

--- view/src/test/java/com/noqapp/mobile/view/controller/open/DeviceControllerITest.java
+++ view/src/test/java/com/noqapp/mobile/view/controller/open/DeviceControllerITest.java
@@ -83,7 +83,7 @@ class DeviceControllerITest extends ITest {
     @Test
     @DisplayName("Check mobile version is supported with flavor. Response Client VACL")
     void isSupportedWithFlavor_Success_NQCL() throws IOException {
-        String version = "1.2.600";
+        String version = "1.2.650";
         String response = deviceController.isSupportedAppVersion(
             new ScrubbedInput(did),
             new ScrubbedInput(deviceType),
@@ -98,7 +98,7 @@ class DeviceControllerITest extends ITest {
     @Test
     @DisplayName("Check mobile version is supported with flavor. Response Merchant Medical VAMH")
     void isSupportedWithFlavor_Success_NQMH() throws IOException {
-        String version = "1.2.600";
+        String version = "1.2.650";
         String response = deviceController.isSupportedAppVersion(
             new ScrubbedInput(did),
             new ScrubbedInput(deviceType),
