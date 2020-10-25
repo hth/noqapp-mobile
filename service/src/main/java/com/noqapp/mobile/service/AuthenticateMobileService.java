19e571ba619d043eac65b0c10a0d90a636cadb74
hth <hth@users.noreply.github.com>
2020-10-25 10:24:51 -0700

    Optimized import

--- service/src/main/java/com/noqapp/mobile/service/AuthenticateMobileService.java
+++ service/src/main/java/com/noqapp/mobile/service/AuthenticateMobileService.java
@@ -12,7 +12,6 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 /**
