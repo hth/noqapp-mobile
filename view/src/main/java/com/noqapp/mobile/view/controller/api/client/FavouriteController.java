185a3363344d95f06c3bbf1c562b495958e72c7b
hth <hth@users.noreply.github.com>
2020-12-25 15:25:56 -0800

    Support adding and removing of favorite

--- view/src/main/java/com/noqapp/mobile/view/controller/api/client/FavouriteController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/api/client/FavouriteController.java
@@ -5,15 +5,18 @@ import static com.noqapp.mobile.view.controller.api.client.TokenQueueAPIControll
 
 import com.noqapp.common.utils.ScrubbedInput;
 import com.noqapp.domain.BizStoreEntity;
-import com.noqapp.domain.json.JsonStoreProductList;
+import com.noqapp.domain.UserPreferenceEntity;
+import com.noqapp.domain.json.JsonResponse;
 import com.noqapp.health.domain.types.HealthStatusEnum;
 import com.noqapp.health.service.ApiHealthService;
+import com.noqapp.mobile.domain.body.client.Feedback;
 import com.noqapp.mobile.service.AuthenticateMobileService;
 import com.noqapp.repository.StoreHourManager;
 import com.noqapp.search.elastic.domain.BizStoreElastic;
 import com.noqapp.search.elastic.domain.FavoriteElastic;
 import com.noqapp.search.elastic.helper.DomainConversion;
 import com.noqapp.service.BizService;
+import com.noqapp.service.UserProfilePreferenceService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +24,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +54,7 @@ public class FavouriteController {
     private BizService bizService;
     private StoreHourManager storeHourManager;
     private AuthenticateMobileService authenticateMobileService;
+    private UserProfilePreferenceService userProfilePreferenceService;
     private ApiHealthService apiHealthService;
 
     @Autowired
@@ -56,11 +62,13 @@ public class FavouriteController {
         BizService bizService,
         StoreHourManager storeHourManager,
         AuthenticateMobileService authenticateMobileService,
+        UserProfilePreferenceService userProfilePreferenceService,
         ApiHealthService apiHealthService
     ) {
         this.bizService = bizService;
         this.storeHourManager = storeHourManager;
         this.authenticateMobileService = authenticateMobileService;
+        this.userProfilePreferenceService = userProfilePreferenceService;
         this.apiHealthService = apiHealthService;
     }
 
@@ -103,6 +111,66 @@ public class FavouriteController {
             LOG.error("Failed getting favorite reason={}", e.getLocalizedMessage(), e);
             methodStatusSuccess = false;
             return favoriteElastic.asJson();
+        } finally {
+            apiHealthService.insert(
+                "/",
+                "favorite",
+                FavouriteController.class.getName(),
+                Duration.between(start, Instant.now()),
+                methodStatusSuccess ? HealthStatusEnum.G : HealthStatusEnum.F);
+        }
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String actionOnFavorite(
+        @RequestHeader("X-R-DID")
+        ScrubbedInput did,
+
+        @RequestHeader ("X-R-DT")
+        ScrubbedInput dt,
+
+        @RequestHeader ("X-R-MAIL")
+        ScrubbedInput mail,
+
+        @RequestHeader ("X-R-AUTH")
+        ScrubbedInput auth,
+
+        @RequestBody
+        FavoriteElastic favoriteElastic,
+
+        HttpServletResponse response
+    ) throws IOException {
+        boolean methodStatusSuccess = true;
+        Instant start = Instant.now();
+        LOG.info("Load favorite API for mail={} auth={} did={} dt={}", mail, AUTH_KEY_HIDDEN, did, dt);
+        String qid = authenticateMobileService.getQueueUserId(mail.getText(), auth.getText());
+        if (authorizeRequest(response, qid)) return null;
+
+        try {
+            UserPreferenceEntity userPreference = userProfilePreferenceService.findByQueueUserId(qid);
+            BizStoreEntity bizStore = bizService.findByCodeQR(favoriteElastic.getCodeQR());
+
+            switch (favoriteElastic.getActionType()) {
+                case ADD:
+                    if (!userPreference.getFavoriteTagged().contains(bizStore.getBizName().getCodeQR())) {
+                        userProfilePreferenceService.addFavorite(qid, favoriteElastic.getCodeQR());
+                    }
+                    break;
+                case REMOVE:
+                    if (userPreference.getFavoriteTagged().contains(bizStore.getBizName().getCodeQR())) {
+                        userProfilePreferenceService.removeFavorite(qid, favoriteElastic.getCodeQR());
+                    }
+                    break;
+                default:
+                    LOG.error("Reached not supported condition {}", favoriteElastic.getActionType());
+                    throw new UnsupportedOperationException("Reached not supported condition");
+            }
+
+            return new JsonResponse(true).asJson();
+        } catch (Exception e) {
+            LOG.error("Failed getting favorite reason={}", e.getLocalizedMessage(), e);
+            methodStatusSuccess = false;
+            return new JsonResponse(false).asJson();
         } finally {
             apiHealthService.insert(
                 "/",
