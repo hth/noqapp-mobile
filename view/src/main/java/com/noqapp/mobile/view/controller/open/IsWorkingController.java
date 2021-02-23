4303ab6bc387d727816185839dfc0df0ea4577ff
hth <hth@users.noreply.github.com>
2021-02-22 16:07:52 -0800

    Change content negotiation to support request with and without json, htm

--- view/src/main/java/com/noqapp/mobile/view/controller/open/IsWorkingController.java
+++ view/src/main/java/com/noqapp/mobile/view/controller/open/IsWorkingController.java
@@ -1,30 +1,12 @@
 package com.noqapp.mobile.view.controller.open;
 
-import com.noqapp.health.domain.json.JsonSiteHealth;
-import com.noqapp.health.domain.json.JsonSiteHealthService;
-import com.noqapp.health.domain.types.HealthStatusEnum;
-import com.noqapp.health.service.ApiHealthService;
-import com.noqapp.service.SiteHealthService;
-
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.time.Duration;
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 
 /**
  * User: hitender
@@ -40,21 +22,6 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping(value = "/open")
 public class IsWorkingController {
     private static final Logger LOG = LoggerFactory.getLogger(IsWorkingController.class);
-
-    /* Set cache parameters. */
-    private final Cache<String, JsonSiteHealth> cache = Caffeine.newBuilder()
-        .maximumSize(1)
-        .expireAfterWrite(15, TimeUnit.MINUTES)
-        .build();
-
-    private SiteHealthService siteHealthService;
-    private ApiHealthService apiHealthService;
-
-    @Autowired
-    public IsWorkingController(SiteHealthService siteHealthService, ApiHealthService apiHealthService) {
-        this.siteHealthService = siteHealthService;
-        this.apiHealthService = apiHealthService;
-    }
 
     /**
      * Supports HTML call.
@@ -73,48 +40,10 @@ public class IsWorkingController {
     @RequestMapping(
         value = "/isWorking",
         method = {RequestMethod.GET, RequestMethod.HEAD},
-        produces = {
-            MediaType.TEXT_HTML_VALUE + ";charset=UTF-8",
-        }
+        produces = {MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"}
     )
     public String isWorking() {
         LOG.info("isWorking");
         return "isWorking";
-    }
-
-    /**
-     * Supports JSON call.
-     *
-     * @return
-     */
-    @GetMapping(
-        value = "/healthCheck",
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public String healthCheck() {
-        Instant start = Instant.now();
-        LOG.info("Health check invoked");
-        JsonSiteHealth jsonSiteHealth = cache.getIfPresent("siteHealth");
-        if (null == jsonSiteHealth) {
-            jsonSiteHealth = new JsonSiteHealth();
-            JsonSiteHealthService jsonHealthService = new JsonSiteHealthService("sw");
-            siteHealthService.doSiteHealthCheck(jsonSiteHealth);
-            jsonHealthService.ended().setHealthStatus(HealthStatusEnum.G);
-            jsonSiteHealth.increaseServiceUpCount();
-            jsonSiteHealth.addJsonHealthServiceChecks(jsonHealthService);
-
-            cache.put("siteHealth", jsonSiteHealth);
-        }
-
-        apiHealthService.insert(
-            "/healthCheck",
-            "healthCheck",
-            IsWorkingController.class.getName(),
-            Duration.between(start, Instant.now()),
-            HealthStatusEnum.G);
-
-        return jsonSiteHealth.asJson();
     }
 }
