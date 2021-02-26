6edfb676f628ffef3ddd45bf145e210dfb2949e2
hth <hth@users.noreply.github.com>
2021-02-26 01:55:13 -0800

    Updated to latest version

--- domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
+++ domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
@@ -32,31 +32,31 @@ public enum LowestSupportedAppEnum {
     VACH("1.2.700",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQCH,
-        "1.2.700",                      //Latest App Version on Play Store
+        "1.2.721",                      //Latest App Version on Play Store
         "Version Android Client HealthCare"),
 
     VAMS("1.2.700",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQMS,
-        "1.2.700",                      //Latest App Version on Play Store
+        "1.2.721",                      //Latest App Version on Play Store
         "Version Android Business Store"),
 
     VAMH("1.2.700",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQMH,
-        "1.2.700",                      //Latest App Version on Play Store
+        "1.2.721",                      //Latest App Version on Play Store
         "Version Android Business HealthCare"),
 
     VAMT("1.2.700",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQMT,
-        "1.2.700",                      //Latest App Version on Play Store
+        "1.2.721",                      //Latest App Version on Play Store
         "Version Android Business TV"),
 
     VAMI("1.2.700",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQMI,
-        "1.2.700",                      //Latest App Version on Play Store
+        "1.2.721",                      //Latest App Version on Play Store
         "Version Android Business Inventory");
 
     private static final Logger LOG = LoggerFactory.getLogger(LowestSupportedAppEnum.class);
