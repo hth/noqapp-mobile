3246ab4b93fe3bc8669b47ec0ae1de196cb3c5d6
hth <hth@users.noreply.github.com>
2021-03-29 22:54:16 -0700

    updated lowest supported

--- domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
+++ domain/src/main/java/com/noqapp/mobile/types/LowestSupportedAppEnum.java
@@ -23,40 +23,40 @@ public enum LowestSupportedAppEnum {
         "Version iPhone"),
     /* 1.1.10 is not released. */
 
-    VACL("1.2.721",                     //Oldest Supported App Version in String
+    VACL("1.2.800",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQCL,
-        "1.2.721",                      //Latest App Version on Play Store
+        "1.2.800",                      //Latest App Version on Play Store
         "Version Android Client"),
 
-    VACH("1.2.722",                     //Oldest Supported App Version in String
+    VACH("1.2.800",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQCH,
-        "1.2.722",                      //Latest App Version on Play Store
+        "1.2.800",                      //Latest App Version on Play Store
         "Version Android Client HealthCare"),
 
-    VAMS("1.2.722",                     //Oldest Supported App Version in String
+    VAMS("1.2.800",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQMS,
-        "1.2.722",                      //Latest App Version on Play Store
+        "1.2.800",                      //Latest App Version on Play Store
         "Version Android Business Store"),
 
-    VAMH("1.2.722",                     //Oldest Supported App Version in String
+    VAMH("1.2.800",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQMH,
-        "1.2.722",                      //Latest App Version on Play Store
+        "1.2.800",                      //Latest App Version on Play Store
         "Version Android Business HealthCare"),
 
-    VAMT("1.2.722",                     //Oldest Supported App Version in String
+    VAMT("1.2.800",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQMT,
-        "1.2.722",                      //Latest App Version on Play Store
+        "1.2.800",                      //Latest App Version on Play Store
         "Version Android Business TV"),
 
-    VAMI("1.2.722",                     //Oldest Supported App Version in String
+    VAMI("1.2.800",                     //Oldest Supported App Version in String
         DeviceTypeEnum.A,
         AppFlavorEnum.NQMI,
-        "1.2.722",                      //Latest App Version on Play Store
+        "1.2.800",                      //Latest App Version on Play Store
         "Version Android Business Inventory");
 
     private static final Logger LOG = LoggerFactory.getLogger(LowestSupportedAppEnum.class);
