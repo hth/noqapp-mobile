d692880679a56702d8825d198a8d5fe933a7850f
hth <hth@users.noreply.github.com>
2021-07-16 13:19:09 -0700

    Test registered user join queue

--- view/src/test/java/com/noqapp/mobile/view/controller/api/client/TokenQueueAPIControllerITest.java
+++ view/src/test/java/com/noqapp/mobile/view/controller/api/client/TokenQueueAPIControllerITest.java
@@ -2,6 +2,8 @@ package com.noqapp.mobile.view.controller.api.client;
 
 import static com.noqapp.common.utils.DateUtil.MINUTES_IN_MILLISECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.noqapp.common.utils.DateUtil;
 import com.noqapp.common.utils.RandomString;
@@ -10,9 +12,15 @@ import com.noqapp.domain.BizNameEntity;
 import com.noqapp.domain.BizStoreEntity;
 import com.noqapp.domain.StoreHourEntity;
 import com.noqapp.domain.UserAccountEntity;
+import com.noqapp.domain.UserProfileEntity;
+import com.noqapp.domain.json.JsonQueue;
 import com.noqapp.domain.json.JsonToken;
+import com.noqapp.domain.json.JsonTokenAndQueue;
+import com.noqapp.domain.json.JsonTokenAndQueueList;
+import com.noqapp.domain.types.AppFlavorEnum;
 import com.noqapp.domain.types.AppointmentStateEnum;
 import com.noqapp.domain.types.DeviceTypeEnum;
+import com.noqapp.domain.types.QueueStatusEnum;
 import com.noqapp.mobile.domain.body.client.JoinQueue;
 import com.noqapp.mobile.domain.body.client.QueueAuthorize;
 import com.noqapp.mobile.domain.body.client.Registration;
@@ -31,6 +39,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -67,7 +76,11 @@ class TokenQueueAPIControllerITest extends ITest {
     private BizStoreEntity bizStore;
     private StoreHourEntity storeHour;
 
+    private UserProfileEntity userProfile;
+    private UserAccountEntity userAccount;
+
     private final int registeredUser = 210;
+    private final int availableTokenCount = 200;
 
     private String timeZone;
 
@@ -108,6 +121,9 @@ class TokenQueueAPIControllerITest extends ITest {
         registerStore();
         registerUsers();
         preAuthorizeUser();
+
+        userProfile = userProfileManager.findOneByPhone("9118000000001");
+        userAccount = userAccountManager.findByQueueUserId(userProfile.getQueueUserId());
     }
 
     /** Registers new users. */
@@ -119,7 +135,7 @@ class TokenQueueAPIControllerITest extends ITest {
     }
 
     private void setBizStoreHours(BizStoreEntity bizStore, StoreHourEntity storeHour) {
-        bizStore.setAverageServiceTime(114000).setAvailableTokenCount(200);
+        bizStore.setAverageServiceTime(114000).setAvailableTokenCount(availableTokenCount);
         bizStore
             .setTimeZone(timeZone)
             .setAppointmentState(AppointmentStateEnum.O);
@@ -246,5 +262,201 @@ class TokenQueueAPIControllerITest extends ITest {
             display.put(String.valueOf(jsonToken.getToken()), jsonToken.getTimeSlotMessage());
             LOG.info("Joined queue {} : {}", jsonToken.getToken(), jsonToken);
         }
+    }
+
+    @Test
+    @DisplayName("Token Queue API executes all APIs")
+    void runThroughAllConditions() throws IOException {
+        getQueueState();
+        getAllJoinedQueues_Before_Join();
+        getAllHistoricalJoinedQueues_Before_Join();
+        joinQueueAll();
+        getAllJoinedQueues_After_Joined();
+        abortQueue();
+        getAllJoinedQueues_After_Abort();
+        allHistoricalJoinedQueues_After_Abort();
+    }
+
+    @DisplayName("Get Queue State")
+    private void getQueueState() throws IOException {
+        BizNameEntity bizName = bizService.findByPhone("9118000000000");
+        BizStoreEntity bizStore = bizService.findOneBizStore(bizName.getId());
+
+        String queueState = tokenQueueAPIController.getQueueState(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(userAccount.getUserId()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            new ScrubbedInput(bizStore.getCodeQR()),
+            httpServletResponse
+        );
+
+        JsonQueue jsonQueue = new ObjectMapper().readValue(queueState, JsonQueue.class);
+        assertEquals(QueueStatusEnum.S, jsonQueue.getQueueStatus());
+    }
+
+    @DisplayName("Get all joined queues before join")
+    private void getAllJoinedQueues_Before_Join() throws IOException {
+        String allJoinedQueues = tokenQueueAPIController.getAllJoinedQueues(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(userAccount.getUserId()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            httpServletResponse
+        );
+        JsonTokenAndQueueList jsonTokenAndQueueList = new ObjectMapper().readValue(allJoinedQueues, JsonTokenAndQueueList.class);
+        assertFalse(jsonTokenAndQueueList.isSinceBeginning());
+        assertEquals(0, jsonTokenAndQueueList.getTokenAndQueues().size());
+    }
+
+    @DisplayName("Get Historical Queues before join")
+    private void getAllHistoricalJoinedQueues_Before_Join() throws IOException {
+        JSONObject jsonObject = new JSONObject()
+            .put("tk", UUID.randomUUID().toString())
+            .put("mo", model)
+            .put("os", osVersion)
+            .put("av", appVersion)
+            .put("dl", "en")
+            .put("ip", "127.0.0.1")
+            .put("lng", "0.0")
+            .put("lat", "0.0");
+
+        String allJoinedQueues = tokenQueueAPIController.allHistoricalJoinedQueues(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(AppFlavorEnum.NQCL.getName()),
+            new ScrubbedInput(userAccount.getUserId()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            jsonObject.toString(),
+            httpServletRequest,
+            httpServletResponse
+        );
+        JsonTokenAndQueueList jsonTokenAndQueueList = new ObjectMapper().readValue(allJoinedQueues, JsonTokenAndQueueList.class);
+        assertFalse(jsonTokenAndQueueList.isSinceBeginning());
+        assertEquals(0, jsonTokenAndQueueList.getTokenAndQueues().size());
+    }
+
+    @DisplayName("Join a Queue")
+    private void joinQueueAll() throws IOException {
+        BizNameEntity bizName = bizService.findByPhone("9118000000000");
+        BizStoreEntity bizStore = bizService.findOneBizStore(bizName.getId());
+
+        JoinQueue joinQueue = new JoinQueue().setQueueUserId(userAccount.getQueueUserId()).setCodeQR(bizStore.getCodeQR());
+        String afterJoin = tokenQueueAPIController.joinQueue(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(userProfile.getEmail()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            joinQueue,
+            httpServletResponse
+        );
+        JsonToken jsonToken = new ObjectMapper().readValue(afterJoin, JsonToken.class);
+        assertEquals(QueueStatusEnum.S, jsonToken.getQueueStatus());
+    }
+
+    @DisplayName("Get all joined queues after join")
+    private void getAllJoinedQueues_After_Joined() throws IOException {
+        String allJoinedQueues = tokenQueueAPIController.getAllJoinedQueues(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(userProfile.getEmail()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            httpServletResponse
+        );
+        JsonTokenAndQueueList jsonTokenAndQueueList = new ObjectMapper().readValue(allJoinedQueues, JsonTokenAndQueueList.class);
+        assertFalse(jsonTokenAndQueueList.isSinceBeginning());
+        assertEquals(1, jsonTokenAndQueueList.getTokenAndQueues().size());
+    }
+
+    @DisplayName("Abort Queue")
+    private void abortQueue() throws IOException {
+        BizNameEntity bizName = bizService.findByPhone("9118000000000");
+        BizStoreEntity bizStore = bizService.findOneBizStore(bizName.getId());
+
+        tokenQueueAPIController.abortQueue(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(userProfile.getEmail()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            new ScrubbedInput(bizStore.getCodeQR()),
+            httpServletRequest,
+            httpServletResponse
+        );
+    }
+
+    @DisplayName("Get all currently joined queues after join")
+    private void getAllJoinedQueues_After_Abort() throws IOException {
+        String allJoinedQueues = tokenQueueAPIController.getAllJoinedQueues(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(userProfile.getEmail()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            httpServletResponse
+        );
+        JsonTokenAndQueueList jsonTokenAndQueueList = new ObjectMapper().readValue(allJoinedQueues, JsonTokenAndQueueList.class);
+        assertFalse(jsonTokenAndQueueList.isSinceBeginning());
+        assertEquals(0, jsonTokenAndQueueList.getTokenAndQueues().size());
+    }
+
+    @DisplayName("All Historical Queues any joined")
+    private void allHistoricalJoinedQueues_After_Abort() throws IOException {
+        JSONObject jsonObject = new JSONObject()
+            .put("tk", UUID.randomUUID().toString())
+            .put("mo", model)
+            .put("os", osVersion)
+            .put("av", appVersion)
+            .put("dl", "en")
+            .put("ip", "127.0.0.1")
+            .put("lng", "0.0")
+            .put("lat", "0.0");
+
+        /* For the first time fetches from beginning. */
+        String allJoinedQueues = tokenQueueAPIController.allHistoricalJoinedQueues(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(AppFlavorEnum.NQCL.getName()),
+            new ScrubbedInput(userAccount.getUserId()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            jsonObject.toString(),
+            httpServletRequest,
+            httpServletResponse
+        );
+        JsonTokenAndQueueList jsonTokenAndQueueList = new ObjectMapper().readValue(allJoinedQueues, JsonTokenAndQueueList.class);
+        assertTrue(jsonTokenAndQueueList.isSinceBeginning());
+        assertEquals(1, jsonTokenAndQueueList.getTokenAndQueues().size());
+        JsonTokenAndQueue jsonTokenAndQueue = jsonTokenAndQueueList.getTokenAndQueues().iterator().next();
+        assertEquals("Dr Aaj Kal", jsonTokenAndQueue.getDisplayName());
+        assertEquals(1, jsonTokenAndQueue.getToken());
+        assertEquals(0, jsonTokenAndQueue.getRatingCount());
+
+        /* On second fetch, its not complete history, gets latest. */
+        allJoinedQueues = tokenQueueAPIController.allHistoricalJoinedQueues(
+            new ScrubbedInput(did),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(AppFlavorEnum.NQCL.getName()),
+            new ScrubbedInput(userAccount.getUserId()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            jsonObject.toString(),
+            httpServletRequest,
+            httpServletResponse
+        );
+        jsonTokenAndQueueList = new ObjectMapper().readValue(allJoinedQueues, JsonTokenAndQueueList.class);
+        assertFalse(jsonTokenAndQueueList.isSinceBeginning());
+        assertEquals(1, jsonTokenAndQueueList.getTokenAndQueues().size());
+
+        /* After changing device Id, it is assumed to be a old user. This person has old history. */
+        allJoinedQueues = tokenQueueAPIController.allHistoricalJoinedQueues(
+            new ScrubbedInput(UUID.randomUUID().toString()),
+            new ScrubbedInput(deviceType),
+            new ScrubbedInput(AppFlavorEnum.NQCL.getName()),
+            new ScrubbedInput(userAccount.getUserId()),
+            new ScrubbedInput(userAccount.getUserAuthentication().getAuthenticationKey()),
+            jsonObject.toString(),
+            httpServletRequest,
+            httpServletResponse
+        );
+        jsonTokenAndQueueList = new ObjectMapper().readValue(allJoinedQueues, JsonTokenAndQueueList.class);
+        assertFalse(jsonTokenAndQueueList.isSinceBeginning());
+        assertEquals(1, jsonTokenAndQueueList.getTokenAndQueues().size());
     }
 }
